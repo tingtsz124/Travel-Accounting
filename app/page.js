0187ec76de63e46bb0545edcdc5e2082b002a61a
tingtsz124 <50259140+tingtsz124@users.noreply.github.com>
2026-09-16 00:16:14 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 'use client';
 
+import './globals.css';
 import React, { useState, useMemo } from 'react';
 import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer, Legend } from 'recharts';
 import { PlusCircle, Wallet, Compass, Sparkles, Receipt, Calculator } from 'lucide-react';
